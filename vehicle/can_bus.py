f0bde09244c0e9da10131761c233d30745b50345
onurizmitlioglu <onurizmitlioglu@gmail.com>
2026-03-28 17:22:35 +0300

    Add ENGINE CAN message catalogue (0x0C0 - 0x0CF)

--- vehicle/can_bus.py
+++ vehicle/can_bus.py
@@ -22,10 +22,10 @@ CAN message structure and bit packing are simulation-specific.
 MESSAGE_CATALOGUE: Dict[int, List[SignalDef]] = {}
 
 '''ENGINE_STATUS_1:
-Engine (RPM)                            [0/8000]
-Throttle (%)                            [0/100]
-Engine Load (%)                         [0/100]
-Ignition Timing (deg)                   [-5/40]
+Engine (RPM)                            [0.00/8000.00]
+Throttle (%)                            [0.0/100.0]
+Engine Load (%)                         [0.0/100.0]
+Ignition Timing (deg)                   [-5.0/40.0]
 '''
 MESSAGE_CATALOGUE[0x0C0] = [
     SignalDef("engine_rpm", 0, 16, 0.25, 0, 0, 8000, "rpm"),
@@ -35,10 +35,10 @@ MESSAGE_CATALOGUE[0x0C0] = [
 ]
 
 '''ENGINE_STATUS_2: 
-Coolant Temperature (°C)                [-40/+215]
-Oil Temperature (°C)                    [-40/+215] 
-Oil Pressure (bar)                      [0/7]
-Mass Air Flow (g/s)                     [0/655.35]
+Coolant Temperature (°C)                [-40.0/+215.0]
+Oil Temperature (°C)                    [-40.0/+215.0] 
+Oil Pressure (bar)                      [0.00/7.00]
+Mass Air Flow (g/s)                     [0.00/655.35]
 Manifold Absolute Pressure (kPa)        [0/255]
 '''
 MESSAGE_CATALOGUE[0x0C1] = [
@@ -47,4 +47,33 @@ MESSAGE_CATALOGUE[0x0C1] = [
     SignalDef("oil_pressure_bar", 18, 10, 0.01, 0, 0, 7, "bar"),
     SignalDef("maf_gs", 28, 16, 0.01, 0, 0, 655.35, "g/s"),
     SignalDef("map_kpa", 44, 8, 1, 0, 0, 255, "kPa"),
+]
+
+'''ENGINE_STATUS_3:
+Fuel Level (%)                          [0.0/100.0]
+Fuel Consumption (l/100km)              [0.0/50.0]
+Injector Pulse Width (µs)               [0/25000]
+Normalized Air-Fuel Ratio (λ)           [0.000/1.990]                                   
+'''
+MESSAGE_CATALOGUE[0x0C2] = [
+    SignalDef("fuel_level_pct", 0, 10, 0.1, 0, 0, 100, "%"),
+    SignalDef("fuel_consumption", 10, 10, 0.1, 0, 0, 50, "l/100km"),
+    SignalDef("injector_pw_us", 20, 15, 1.0, 0, 0, 25000, "µs"),
+    SignalDef("lambda", 35, 11, 0.001, 0, 0, 1.99, "λ"),
+]
+
+'''
+ENGINE_FAULT:
+Malfunction Indicator Lamp (binary)     [0/1]
+Active DTC Count                        [0/127]
+P0300 - Random/Multiple Misfire         [0/1]
+p0171 - Fuel System Lean (Bank 1)       [0/1]
+P0217 - Engine Coolant Over Temp.       [0/1]
+'''
+MESSAGE_CATALOGUE[0x0CF] = [
+    SignalDef("mil_status", 0, 1, 1.0, 0, 0, 1, ""),
+    SignalDef("dtc_count", 1, 7, 1.0, 0, 0, 127, ""),
+    SignalDef("dtc_p0300", 8, 1, 1.0, 0, 0, 1, ""),
+    SignalDef("dtc_p0171", 9, 1, 1.0, 0, 0, 1, ""),
+    SignalDef("dtc_p0217", 10, 1, 1.0, 0, 0, 1, ""),
 ]
